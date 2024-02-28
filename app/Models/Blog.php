7f194568100708b9f4188a18575d8544810d2c80
victor <valencia_vik@hotmail.com>
2024-02-28 16:15:33 -0600

    First api demo

---
+++ app/Models/Blog.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+class Blog
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'body'        
+    ];
+   
+}
